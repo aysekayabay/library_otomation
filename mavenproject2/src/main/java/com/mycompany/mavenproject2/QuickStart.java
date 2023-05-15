f6d488cb09909479917d613dc61c415b442ca8c2
Ibrahim Sahin <ibrahim2001sahin@hotmail.com>
2023-05-15 15:36:49 +0300

    Registration

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/QuickStart.java
@@ -0,0 +1,35 @@
+package com.mycompany.mavenproject2;
+
+import static com.mongodb.client.model.Filters.eq;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class QuickStart {
+    public static void main( String[] args ) {
+
+        String uri = "mongodb+srv://Ibrahim:ibrahimU123@cluster0.y3msch8.mongodb.net/Registered?retryWrites=true&w=majority";
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("Registered");
+            MongoCollection<Document> collection = database.getCollection("users");
+            MongoCursor<Document> cursor = collection.find().iterator();
+            while (cursor.hasNext()) {
+                Document document = cursor.next();
+                System.out.println(document.toJson());
+            }
+
+            Document doc = collection.find(eq("name", "İbrahim Şahin")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
+}
